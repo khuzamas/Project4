4c0b4749ac3c925865ec4987c6a52ce4fe8b1fdd
Alshemimri <Alshemimri@gmail.com>
2019-05-08 11:50:21 +0300

    Server.js added

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT
+
+// Connect Mongoose
+mongoose.connect('mongodb://localhost/project4db',{
+    useNewUrlParser: true
+})
+.then(()=>{
+    console.log('MongoDB Connected');
+})
+.catch((err)=> console.log(err))
+
+// Body parser middleware
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+
+// 
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+});
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+})
