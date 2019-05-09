1eb00671a524f0eb844e4ca9b1001c664fdf5877
Alshemimri <Alshemimri@gmail.com>
2019-05-09 11:34:10 +0300

    user model added

--- models/user.js
+++ models/user.js
@@ -15,15 +15,47 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
     password: {
         type: String,
         required: true
     },
+    dateofbirth: {
+        type: Date,
+        default: Date.now
+    },
+    friend: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    icon: {
+        type: String
+    },
+    heigh: {
+        type: String
+    },
+    weight: {
+        type: String
+    },
+    activity: {
+        type: String
+    },
+    goal: {
+        type: String
+    },
+    challenges: [{ type: Schema.Types.ObjectId, ref: 'Challenge' }],
+    badges: [{ type: Schema.Types.ObjectId, ref: 'Badge' }],
+    tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+    points: {
+        type: Number,
+        default: 0
+    },
     date: {
         type: Date,
         default: Date.now
-    }
+    },
 });
 
 
-mongoose.model('users', UserSchema);
+const User = mongoose.model('User',UserSchema)
+module.exports = User
