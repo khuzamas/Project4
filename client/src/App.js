5b5c4c85ad07d23986eb009964860558d1b1caf9
khuzamas <khuzamshu@gmail.com>
2019-05-09 13:52:34 +0300

    css updated

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,23 @@ function App() {
   return (
     <div className="app">
       <div className="navbar">
-
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="home">Home</a>
+          </li>
+        </ul>
       </div>
       
       <div className="body3">
-      world
+        <div className="tasks">
+          Daily tasks
+        </div>
+        <div className="lead">
+          Leader board
+        </div>
+        <div className="challenges">
+          Active challenges
+        </div>
       </div>
       
     </div>
