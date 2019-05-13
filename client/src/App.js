9b00fb8f4f38f52922a299b194cfa783a5a92be5
khuzamas <khuzamshu@gmail.com>
2019-05-13 10:37:36 +0300

    daily tasks list design

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="app">
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state= {
+    check: false
+  }
+
+  checkHandler= () => {
+    console.log(this.state.check);
+    this.setState({check: !(this.state.check)})
+  }
+
+  
+  render() {
+    const checkShow= this.state.check === false ? " inactive" : " is-active"
+    return (
+      <div className="app">
       <div className="navbar">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="home">Home</a>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="home">Home</a>
           </li>
         </ul>
       </div>
       
       <div className="body3">
-        <div className="tasks">
-          Daily tasks
+        <div className="tasks card">
+          <div className="card-header">
+            Daily Tasks
+          </div>
+          <div className="card-body">
+            <td className="td img" onClick={() => this.checkHandler()} ><img src="https://img.icons8.com/cotton/64/000000/checkmark.png" alt="" className={"check"+checkShow}/></td>
+            <td className="td">Mark</td>  
+          </div>
         </div>
         <div className="lead">
           Leader board
@@ -25,7 +44,6 @@ function App() {
       </div>
       
     </div>
-  );
+    )
+  }
 }
-
-export default App;
